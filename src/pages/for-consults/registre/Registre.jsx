9996ac5ac3af2b5609667b1f5feca0e98e6be83c
abdusamadweb <abdusamadweb@gmail.com>
2023-12-05 13:51:01 +0500

    img

--- src/pages/for-consults/registre/Registre.jsx
+++ src/pages/for-consults/registre/Registre.jsx
@@ -30,7 +30,7 @@ const Registre = ({ univer, title, inpTitle, inpPlaceholder }) => {
         e.preventDefault()
 
         const formData = new FormData()
-        formData.append('passportFile', JSON?.stringify([passportFile?.id]))
+        formData.append('passportFile', JSON?.stringify(passportFile))
         formData.append('companyName', companyName)
         formData.append('fullName', name)
         formData.append('password', password)
